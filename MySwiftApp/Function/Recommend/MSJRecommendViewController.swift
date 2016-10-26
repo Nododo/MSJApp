13bf9a99e3e546fdced364835a53b6d8a7516f4e
DWX <nododo1027@gmail.com>
2016-10-26 11:31:45 +0800

    fix the recommend bug

--- MySwiftApp/Function/Recommend/MSJRecommendViewController.swift
+++ MySwiftApp/Function/Recommend/MSJRecommendViewController.swift
@@ -11,27 +11,59 @@ import UIKit
 let testIdentifier = "testIdentifier"
 let testHeaderIdentifier = "testHeaderIdentifier"
 
-class MSJRecommendViewController: MSJBaseViewController, MSJRecommendFlowLayoutDelegate, UICollectionViewDataSource {
+class MSJRecommendViewController: MSJBaseViewController, CHTCollectionViewDelegateWaterfallLayout, UICollectionViewDataSource {
+
     @IBOutlet weak var mainView: UICollectionView!
     
     override func viewDidLoad() {
         //MARK: self.automaticallyAdjustsScrollViewInsets在storyboard中设置为NO
         super.viewDidLoad()
+        mainView.delegate = self;
+        mainView.dataSource = self;
+        
+        let layout = CHTCollectionViewWaterfallLayout()
+        mainView.collectionViewLayout = layout
+        
         mainView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: testIdentifier)
-        mainView.register(MSJRecommendTopReusableView.self, forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: testHeaderIdentifier)
+        mainView.register(MSJRecommendTopReusableView.self, forSupplementaryViewOfKind: CHTCollectionElementKindSectionHeader, withReuseIdentifier: testHeaderIdentifier)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    
-    func collectionView (collectionView: UICollectionView,layout collectionViewLayout: UICollectionViewLayout,
-                         sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize{
+    func collectionView (_ collectionView: UICollectionView,layout collectionViewLayout: UICollectionViewLayout,
+                         sizeForItemAtIndexPath indexPath: IndexPath) -> CGSize {
         return CGSize(width: 100, height: 100)
     }
-
+    
+     func collectionView (_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout,
+                          heightForHeaderInSection section: NSInteger) -> CGFloat {
+        return 200
+    }
+    
+    func collectionView (_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout,
+                         heightForFooterInSection section: NSInteger) -> CGFloat {
+        return 0
+    }
+    
+     func collectionView (_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout,
+                          insetForSectionAtIndex section: NSInteger) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 5, bottom: 0, right: 5)
+    }
+    
+     func collectionView (_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout,
+                          minimumInteritemSpacingForSectionAtIndex section: NSInteger) -> CGFloat {
+        return 0
+    }
+    
+     func collectionView (_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout,
+                          columnCountForSection section: NSInteger) -> NSInteger {
+        return 2
+    }
+    
+    
     public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 10
     }
@@ -42,29 +74,12 @@ class MSJRecommendViewController: MSJBaseViewController, MSJRecommendFlowLayoutD
         return cell
     }
     
-    func collectionView (collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout,
-    minimumInteritemSpacingForSectionAtIndex section: NSInteger) -> CGFloat{
-    return 0
+    public func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        if kind == CHTCollectionElementKindSectionHeader {
+            let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: testHeaderIdentifier, for: indexPath);
+            header.backgroundColor = UIColor.randomColor()
+            return header
+        }
+        return UICollectionReusableView()
     }
-    
-    func collectionView (collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout,
-                         heightForHeaderInSection section: NSInteger) -> CGFloat{
-        return 100
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
-        let view = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionElementKindSectionHeader, withReuseIdentifier: testHeaderIdentifier, for: indexPath)
-        view.backgroundColor = UIColor.randomColor()
-        return view
-    }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
