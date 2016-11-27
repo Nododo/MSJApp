a78a98e7ef39ea5d8c4393632ddfb0ed95fa63ed
nododo <nododo1027@gmail.com>
2016-11-27 10:17:18 +0800

    add first section cell method

--- MySwiftApp/Function/Recommend/MSJRecommendFirstSectionCell.swift
+++ MySwiftApp/Function/Recommend/MSJRecommendFirstSectionCell.swift
@@ -10,9 +10,25 @@ import UIKit
 
 class MSJRecommendFirstSectionCell: UICollectionViewCell {
 
+    @IBOutlet weak var btn1: UIButton!
+    @IBOutlet weak var btn2: UIButton!
+    @IBOutlet weak var btn3: UIButton!
+    @IBOutlet weak var btn4: UIButton!
+    @IBOutlet weak var btn5: UIButton!
+    @IBOutlet weak var btn6: UIButton!
+    
+    var tapBlock: ((NSInteger) ->())? = nil
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        btn1.addTarget(self, action: #selector(tapBtn(btn:)), for: .touchUpInside)
+        btn2.addTarget(self, action: #selector(tapBtn(btn:)), for: .touchUpInside)
+        btn3.addTarget(self, action: #selector(tapBtn(btn:)), for: .touchUpInside)
+        btn4.addTarget(self, action: #selector(tapBtn(btn:)), for: .touchUpInside)
+        btn5.addTarget(self, action: #selector(tapBtn(btn:)), for: .touchUpInside)
+        btn6.addTarget(self, action: #selector(tapBtn(btn:)), for: .touchUpInside)
     }
 
+    func tapBtn(btn: UIButton) {
+        
+    }
 }
