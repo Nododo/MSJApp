4eeba14fe5eb761e5330f17f5f5bfd6ae0cfbfcd
dwx <duweixin@coohua.com>
2016-12-08 18:16:14 +0800

    添加广告轮播 待完成

---
+++ MySwiftApp/Tool/MSJCycleAdView.swift
@@ -0,0 +1,21 @@
+//
+//  MSJCycleAdView.swift
+//  MySwiftApp
+//
+//  Created by coohua on 16/12/8.
+//  Copyright © 2016年 Nododo. All rights reserved.
+//
+
+import UIKit
+
+class MSJCycleAdView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
