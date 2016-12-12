0de82df18bb5440f6719718cb750eabac49864e0
dwx <duweixin@coohua.com>
2016-12-12 19:42:46 +0800

    广告轮播banner

--- MySwiftApp/Tool/MSJCycleAdView.swift
+++ MySwiftApp/Tool/MSJCycleAdView.swift
@@ -9,13 +9,37 @@
 import UIKit
 
 class MSJCycleAdView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    var collectionView: UICollectionView!
+    var pageControl: UIPageControl!
+    var pictures: [String]!
+    
+    public init(frame: CGRect, pictures: [String]!) {
+        self.pictures = pictures
+        super.init(frame: frame)
+        setupSubviews()
     }
-    */
-
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupSubviews() {
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = self.bounds.size
+        layout.minimumInteritemSpacing = 0
+        layout.minimumLineSpacing = 0
+        collectionView = UICollectionView(frame: CGRect.zero, collectionViewLayout: layout)
+        addSubview(collectionView)
+        
+        pageControl = UIPageControl()
+        pageControl.numberOfPages = self.pictures.count
+        addSubview(pageControl)
+    }
+    
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+    }
 }
+
