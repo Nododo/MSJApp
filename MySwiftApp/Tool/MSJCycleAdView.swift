216379eb1fe4bd223298c3266e00980362ec18bd
dwx <duweixin@coohua.com>
2016-12-13 19:41:30 +0800

    广告页 添加timer 添加代码

--- MySwiftApp/Tool/MSJCycleAdView.swift
+++ MySwiftApp/Tool/MSJCycleAdView.swift
@@ -7,17 +7,23 @@
 //
 
 import UIKit
+import SwiftyTimer
 
-class MSJCycleAdView: UIView {
+class MSJCycleAdView: UIView, UICollectionViewDelegate, UICollectionViewDataSource {
     
     var collectionView: UICollectionView!
-    var pageControl: UIPageControl!
     var pictures: [String]!
+    var pictureNames: [String]?
+    
+    var pageControl: UIPageControl!
+    var nameLabel: UILabel!
+    var timer: Timer!
     
     public init(frame: CGRect, pictures: [String]!) {
         self.pictures = pictures
         super.init(frame: frame)
         setupSubviews()
+        setupTimer()
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -30,16 +36,58 @@ class MSJCycleAdView: UIView {
         layout.minimumInteritemSpacing = 0
         layout.minimumLineSpacing = 0
         collectionView = UICollectionView(frame: CGRect.zero, collectionViewLayout: layout)
+        collectionView.delegate = self
+        collectionView.dataSource = self
         addSubview(collectionView)
         
         pageControl = UIPageControl()
         pageControl.numberOfPages = self.pictures.count
         addSubview(pageControl)
+        
+        nameLabel = UILabel()
+        addSubview(nameLabel)
     }
     
+    func setupTimer() {
+        timer = Timer.every(0.3, { 
+            self.scrollToNext()
+        })
+    }
+    
+    func scrollToNext()  {
+        //为collectionview 添加extention 或者protocol  scroll to next index
+    }
     
     override func layoutSubviews() {
         super.layoutSubviews()
+        collectionView.snp.makeConstraints { (make) in
+            make.edges.equalToSuperview()
+        }
+        
+        pageControl.snp.makeConstraints { (make) in
+            make.centerX.equalToSuperview()
+            make.bottom.equalToSuperview()
+            make.height.equalTo(30)
+            make.width.equalTo(50)
+        }
+        
+        nameLabel.snp.makeConstraints { (make) in
+            make.top.left.right.equalToSuperview()
+            make.height.equalTo(30)
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.pictures.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        let currentIndex = Int (scrollView.contentOffset.x / collectionView.bounds.width)
+        pageControl.currentPage = currentIndex
     }
 }
 
